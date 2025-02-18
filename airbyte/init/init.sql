5bcf1cf6ea1887931e1d9421edf51eb84c2bbb5b
tujrg <138572862+tujrg@users.noreply.github.com>
2025-02-18 08:00:14 +0700

    Add docker compose files for tools

---
+++ airbyte/init/init.sql
@@ -0,0 +1,11 @@
+-- Create a temporal and set the owner
+CREATE DATABASE airbyte_metadata_db OWNER postgres;
+
+-- Create a temporal and set the owner
+CREATE DATABASE temporal OWNER postgres;
+
+-- Create a temporal_visibility and set the owner
+CREATE DATABASE temporal_visibility OWNER postgres;
+
+-- Create airflow database
+CREATE DATABASE airflow OWNER postgres;
